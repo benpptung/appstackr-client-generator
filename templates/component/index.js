f6e8dd87bc42a460e605b690cc17570e8e6c7025
benpptung <benpptung@tacol.biz>
2017-05-27 17:11:00 +0800

    remove buggy material-ui

--- templates/component/index.js
+++ templates/component/index.js
@@ -7,7 +7,7 @@ const React = require('react');
 const inherits = require('inherits');
 const _proto = require('./prototype');
 const inject = require('inject-css');
-const files = require('./style/images.files');
+const files = require('./css/images.files');
 
 
 /**
@@ -35,12 +35,12 @@ prototype.render = function() {
   return (
 
     /*jshint ignore:start*/
-    <div>
+    <div className="{component_name_lowercase}">
       <h1>React Starter kit</h1>
       <p>
         Powerful frontend React Component Starter Kit.<br />
         Support jsx, js, css, scss, styl, and automatically move image files in other appstackr projects.<br />
-        <a href="http://material-ui.com">material-ui</a> and <a href="https://www.npmjs.com/package/react-tap-event-plugin">react-tap-event-plugin</a> ready.
+        <a href="https://www.npmjs.com/package/react-tap-event-plugin">react-tap-event-plugin</a> ready.
       </p>
       <div><img src="/img/clipart/img.png" width="150" height="205" /></div>
       <p className="footer">Powered by <a href="https://github.com/benpptung/appstackr">appstackr</a></p>
